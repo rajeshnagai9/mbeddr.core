da3d25c3c9de682e297e3adfc725ee6864d53017
Kolja Dummann <kolja.dummann@logv.ws>
2015-05-15 16:20:17 +0200

    updates to dockers files

--- tools/docker/runbuild.bash
+++ tools/docker/runbuild.bash
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 CODE_ROOT="/mbeddr-source"
-MPS_LOCATION="/mbeddr-source/MPS/MPS-mbeddr"
+MPS_LOCATION="/mps"
 TEMP="/tmp"
+BUILD_DIR="/build"
+ARTIFACTS_OUTPUT="/artifacts"
 
 CODE_LANGUAGES="/mbeddr-source/code/languages"
 
-cd "$CODE_ROOT"
-
-ant -f download.xml
+mkdir -p "$BUILD_DIR"
 
 cd "$CODE_LANGUAGES"
 
-ant -Dmps.home="$MPS_LOCATION" -Dmps.platform.caches="$TEMP" -Dmbeddr.github.core.home="$CODE_ROOT"
+ant -Dmps.home="$MPS_LOCATION" -Dmps.platform.caches="$TEMP" -Dmbeddr.github.core.home="$CODE_ROOT" -Dbuild.dir="$BUILD_DIR"
+
+cd "$BUILD_DIR"/artifacts
+
+mv -f **/*.zip "$ARTIFACTS_OUTPUT"
